62240c6c18fe752bf95a31043d967cc30e6eac3b
Arne Mueller <mueller_arne@gmx.net>
2024-02-05 08:59:48 +0000

    gh action and dagger flow

---
+++ ci/test_base.py
@@ -0,0 +1,34 @@
+"""Execute a command."""
+
+import sys
+
+import anyio
+import dagger
+
+
+async def test():
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+        src = client.host().directory(".")
+        python = (
+            client.container()
+            # pull container
+            .from_("python:3.11-slim-bullseye")
+            # mount source directory
+            .with_directory("/ws", src)
+            # change working directory
+            .with_workdir("/ws")
+            # install package and test dependencies
+            .with_exec(["pip", "install", ".[test]"])
+            .with_exec(["pytest", "tests"])
+        )
+
+        print("Starting tests for Python")
+
+        # execute
+        await python.sync()
+
+        print("Tests for Python succeeded!")
+
+
+if __name__ == "__main__":
+    anyio.run(test)
