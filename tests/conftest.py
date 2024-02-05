fbaeed7bc342a657ea8487e574764e8828be599a
Arne Mueller <mueller_arne@gmx.net>
2024-02-05 10:27:58 +0000

    use different names for objects

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,21 +5,21 @@ import pytest
 # Loading mock data for testing
 @pytest.fixture(scope="session")
 def mock_data():
-    data = pd.DataFrame(
+    mock_data = pd.DataFrame(
         {
             "userId": [1, 1, 2, 2, 3],
             "movieId": [101, 102, 101, 103, 102],
             "rating": [5, 4, 3, 2, 1],
         }
     )
-    return data
+    return mock_data
 
 
 # Loading mock data for testing
 @pytest.fixture(scope="session")
 def sample_data():
-    data = pd.read_csv("tests/resources/processed_ratings.csv")
-    return data
+    sample_data = pd.read_csv("tests/resources/processed_ratings.csv")
+    return sample_data
 
 
 # Creating mock data for testing
@@ -38,7 +38,7 @@ def mock_gold_data():
 # Creating mock data for testing
 @pytest.fixture(scope="session")
 def mock_raw_data():
-    data = pd.DataFrame(
+    mock_raw_data = pd.DataFrame(
         {
             "userId": [1, 1, 2, 2, 3, 4, 5],
             "movieId": [101, 102, 101, 103, 102, None, 104],
@@ -46,11 +46,11 @@ def mock_raw_data():
         }
     )
 
-    return data
+    return mock_raw_data
 
 
 # Loading mock data for testing
 @pytest.fixture(scope="session")
 def sample_raw_data():
-    data = pd.read_csv("tests/resources/ratings.csv")
-    return data
+    raw_data = pd.read_csv("tests/resources/ratings.csv")
+    return raw_data
